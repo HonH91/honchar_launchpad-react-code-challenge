878817a862a34d84210c5fd48716ef038b4df75d
HonH91 <olddickinson@gmail.com>
2023-04-15 17:42:57 -0600

    Universities page done
    Search on the Universities page allows to display data coming from the API.

--- src/asynAction/postalLookup.js
+++ src/asynAction/postalLookup.js
@@ -3,38 +3,13 @@ import React from "react";
 
 import { useDispatch } from 'react-redux';
 
-//
 
-
-import {getPostalInfo} from "../store/postalReducer";
+import { getPostalInfo } from "../store/postalReducer";
 
 export const fetchPostal = () => {
   return function (dispatch) {
     fetch(`https://api.zippopotam.us/us/99501`)
-    .then (response => response.json())
-    .then (json => dispatch(getPostalInfo(json)))
+      .then(response => response.json())
+      .then(json => dispatch(getPostalInfo(json)))
   }
 }
-
-
-//function dispatch(){
-    //const dispatch = useDispatch();
-//}
-
-
-
-
-function load() {
-let apiUrl = `https://api.zippopotam.us/us/99501`;
-}
-if (loaded) {
-   
-      <div>
-    </div>
-
-    
-  } else {
-    load();
-
-    return null;
-  }*/
--- src/pages/postal.js
+++ src/pages/postal.js
@@ -2,29 +2,22 @@ import React from 'react';
 
 const PostalLookup = () => {
 
-       async function getData(url) {
-        return  fetch(`https://api.zippopotam.us/us/` + url)
-          .then(response => response.json())
-          .then(json => {return json })
-      }
+    async function getData(url) {
+        return fetch(`https://api.zippopotam.us/us/` + url)
+            .then(response => response.json())
+            .then(json => { return json })
+    }
 
 
     async function handleClick(event) {
         event.preventDefault();
         let input = document.querySelector("#postal");
-       const data = await getData(input.value);
-       console.log( data );
-       document.getElementById("result").innerHTML = data.country + "</br>  " + data["country abbreviation"]
-       + "</br>  " + data["post code"]
-       + "</br>  " + data.places[0]["place name"]
-       + "</br>  " + data.places[0]["state"];
-    }
-
-
-    function searchPostal(event) {
-        event.preventDefault();
-        let input = document.querySelector("#postal");
-        console.log(input.value);
+        const data = await getData(input.value);
+        console.log(data);
+        document.getElementById("result").innerHTML = data.country + "</br>  " + data["country abbreviation"]
+            + "</br>  " + data["post code"]
+            + "</br>  " + data.places[0]["place name"]
+            + "</br>  " + data.places[0]["state"];
     }
 
     return (
@@ -36,7 +29,7 @@ const PostalLookup = () => {
             <h1>Postal Lookup</h1>
 
             <form class="d-flex" role="search">
-                <input class="form-control me-2"  placeholder="Search" aria-label="Search" id="postal" type="number" />
+                <input class="form-control me-2" placeholder="Search" aria-label="Search" id="postal" type="number" />
                 <button class="btn btn-outline-success" type="submit" onClick={handleClick}>Search</ button>
             </form>
 
@@ -47,8 +40,5 @@ const PostalLookup = () => {
         </div>
     );
 };
-
-
-
 
 export default PostalLookup; 
